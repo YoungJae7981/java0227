ec7ba1d56d12264b2021a343c60e8de1b4933673
sample <youngjae7981@gmail.com>
2023-02-28 09:13:48 +0900

    java days-9

--- ContactForm/app.js
+++ ContactForm/app.js
@@ -1,3 +1,28 @@
 let sendButton = document.getElementById("send");
 let resetButton = document.getElementById("reset");
 let form = document.getElementById("form");
+
+form.addEventListener("click", function (e) {
+  e.preventDefault();
+});
+
+resetButton.addEventListener("click", function () {
+  const name = document.getElementById("name");
+  const email = document.getElementById("email");
+  const message = document.getElementById("message");
+  localStorage.removeItem("name");
+  localStorage.removeItem("email");
+  localStorage.removeItem("message");
+  name.value = "";
+  email.value = "";
+  message.value = "";
+});
+
+sendButton.addEventListener("click", function () {
+  const name = document.getElementById("name").value;
+  const email = document.getElementById("email").value;
+  const message = document.getElementById("message").value;
+  localStorage.setItem("name", name);
+  localStorage.setItem("email", email);
+  localStorage.setItem("message", message);
+});
